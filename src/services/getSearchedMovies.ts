38777d5ae9f0e4623b6fa121876f1705649c30c6
Jose Lopez <gollodev@gmail.com>
2023-10-09 04:02:26 -0400

    fix url request to https

--- src/services/getSearchedMovies.ts
+++ src/services/getSearchedMovies.ts
@@ -1,6 +1,5 @@
-// http://www.omdbapi.com/?apikey=817eb860&s=inception
 import { type MoviesResult } from "../types/movies"
-const URL_BASE = 'http://www.omdbapi.com'
+const URL_BASE = 'https://www.omdbapi.com'
 const API_KEY = '817eb860' // apikey should keep hidden in a .env file, this is just for demo purposes.
 const SEARCH_MOVIES = `${URL_BASE}/?apikey=${API_KEY}`
 
